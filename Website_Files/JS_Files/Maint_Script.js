6455b6f42229de5bfecf7d3c40c1b01d78374d12
Mytrexa Company <124737397+Mytrexa-Company@users.noreply.github.com>
2023-02-07 23:32:57 +0200

    Add files via upload

---
+++ Website_Files/JS_Files/Maint_Script.js
@@ -0,0 +1,49 @@
+function Servr_Select1() {
+    document.getElementById("Box1").classList.add('service-box_Anim');
+    document.getElementById("Box1").classList.remove('service-box');
+    document.getElementById("icon1").classList.add('service-icon_Anim');
+    document.getElementById("icon1").classList.remove('service-icon');
+    Maint_Audio.load();
+    Maint_Audio.play();
+    }
+    function Servr_Select2() {
+        document.getElementById("Box2").classList.add('service-box_Anim');
+        document.getElementById("Box2").classList.remove('service-box');
+        document.getElementById("icon2").classList.add('service-icon_Anim');
+        document.getElementById("icon2").classList.remove('service-icon');
+        Maint_Audio.load();
+        Maint_Audio.play();
+        }
+        function Servr_Select3() {
+            document.getElementById("Box3").classList.add('service-box_Anim');
+            document.getElementById("Box3").classList.remove('service-box');
+            document.getElementById("icon3").classList.add('service-icon_Anim');
+            document.getElementById("icon3").classList.remove('service-icon');
+            Maint_Audio.load();
+            Maint_Audio.play();
+            }
+            function Servr_Select4() {
+                document.getElementById("Box4").classList.add('service-box_Anim');
+                document.getElementById("Box4").classList.remove('service-box');
+                document.getElementById("icon4").classList.add('service-icon_Anim');
+                document.getElementById("icon4").classList.remove('service-icon');
+                Maint_Audio.load();
+                Maint_Audio.play();
+                }
+                function Servr_Select5() {
+                    document.getElementById("Box5").classList.add('service-box_Anim');
+                    document.getElementById("Box5").classList.remove('service-box');
+                    document.getElementById("icon5").classList.add('service-icon_Anim');
+                    document.getElementById("icon5").classList.remove('service-icon');
+                    Maint_Audio.load();
+                    Maint_Audio.play();
+                    }
+                    function Servr_Select6() {
+                        document.getElementById("Box6").classList.add('service-box_Anim');
+                        document.getElementById("Box6").classList.remove('service-box');
+                        document.getElementById("icon6").classList.add('service-icon_Anim');
+                        document.getElementById("icon6").classList.remove('service-icon');
+                        Maint_Audio.load();
+                        Maint_Audio.play();
+                        }
+                    
